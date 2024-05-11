dbdd3b714ba2119264df39b00e45156e5165f2e9
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-05-11 19:36:55 +0100

    add contextual toolbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,6 +57,20 @@ export default function Home() {
             Dynamic Settings from UI Labs by mrnest
           </p>
         </Link>
+        <Link
+          href={"/02"}
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            02{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Contextual Toolbar from UI Labs by mrnest
+          </p>
+        </Link>
       </div>
     </main>
   );
