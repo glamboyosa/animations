21789c57276a55610cb14e9c8b9af355b63dacbe
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-07-13 12:51:24 +0100

    add to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,6 +113,20 @@ export default function Home() {
             Directionally aware two-step popover
           </p>
         </Link>
+        <Link
+          href={"/06"}
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            06{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Payments Card Calculator(?)
+          </p>
+        </Link>
       </div>
     </main>
   );
