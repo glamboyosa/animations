aa5df8df62b7e5aa9c7a8cfbb00841ad24d2e94c
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-08-24 12:21:38 +0100

    subtly improve the vaul airbnb share i.e. predictability

--- src/app/04/page.tsx
+++ src/app/04/page.tsx
@@ -17,6 +17,7 @@ import Mike from "../../../public/mike-von-GrfbQPPYguU-unsplash.jpg";
 import Digital from "../../../public/digital-marketing-agency-ntwrk-g39p1kDjvSY-unsplash.jpg";
 const MotionImage = motion(Image);
 const images = [Andrea, Karsten, Merrit];
+console.log(images);
 const translates = [
 	{ x: 0, y: 0 },
 	{ x: -20, y: 20 },
@@ -46,6 +47,7 @@ const Airbnbsharebookmarks = () => {
 	const [loaded, setLoaded] = useState<
 		"not-loaded" | "loaded"
 	>("not-loaded");
+	const [isOpen, setIsOpen] = useState(false);
 	const [container, setContainer] =
 		useState(null);
 	useEffect(() => {
@@ -53,19 +55,15 @@ const Airbnbsharebookmarks = () => {
 	}, []);
 	if (loaded !== "loaded") return null;
 	return (
-		<div
-			style={{
-				transform:
-					"translateX(calc(100vw - 50%))",
-			}}
-			className="h-screen overflow-scroll p-20"
-		>
+		<div className="relative h-screen overflow-scroll p-20">
 			<div
 				ref={
 					setContainer as unknown as LegacyRef<HTMLDivElement>
 				}
 				style={{
 					height: "calc(100vh - 80px)",
+					transform:
+						"translateX(calc(100% - 50%))",
 				}}
 				className="relative max-w-xl overflow-hidden rounded-md bg-white p-10 shadow-black/30 shadow-lg"
 			>
@@ -82,43 +80,50 @@ const Airbnbsharebookmarks = () => {
 						</span>
 					</button>
 
-					<Drawer.Root>
-						<Drawer.Trigger>
-							{" "}
-							<button
-								type="button"
-								className="flex items-center gap-1.5 rounded-2xl border border-black/5 px-3.5 py-2.5 hover:bg-stone-100"
+					<button
+						type="button"
+						className="flex items-center gap-1.5 rounded-2xl border border-black/5 px-3.5 py-2.5 hover:bg-stone-100"
+						onClick={() => setIsOpen(true)}
+					>
+						<span className="text-xs tracking-tighter">
+							Share
+						</span>
+						<span className="text-xs tracking-tighter">
+							<svg
+								width="15"
+								height="15"
+								viewBox="0 0 15 15"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
 							>
-								<span className="text-xs tracking-tighter">
-									Share
-								</span>
-								<span className="text-xs tracking-tighter">
-									<svg
-										width="15"
-										height="15"
-										viewBox="0 0 15 15"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<title>svg</title>
-										<path
-											d="M3.5 5.00006C3.22386 5.00006 3 5.22392 3 5.50006L3 11.5001C3 11.7762 3.22386 12.0001 3.5 12.0001L11.5 12.0001C11.7761 12.0001 12 11.7762 12 11.5001L12 5.50006C12 5.22392 11.7761 5.00006 11.5 5.00006L10.25 5.00006C9.97386 5.00006 9.75 4.7762 9.75 4.50006C9.75 4.22392 9.97386 4.00006 10.25 4.00006L11.5 4.00006C12.3284 4.00006 13 4.67163 13 5.50006L13 11.5001C13 12.3285 12.3284 13.0001 11.5 13.0001L3.5 13.0001C2.67157 13.0001 2 12.3285 2 11.5001L2 5.50006C2 4.67163 2.67157 4.00006 3.5 4.00006L4.75 4.00006C5.02614 4.00006 5.25 4.22392 5.25 4.50006C5.25 4.7762 5.02614 5.00006 4.75 5.00006L3.5 5.00006ZM7 1.6364L5.5682 3.0682C5.39246 3.24393 5.10754 3.24393 4.9318 3.0682C4.75607 2.89246 4.75607 2.60754 4.9318 2.4318L7.1818 0.181802C7.26619 0.09741 7.38065 0.049999 7.5 0.049999C7.61935 0.049999 7.73381 0.09741 7.8182 0.181802L10.0682 2.4318C10.2439 2.60754 10.2439 2.89246 10.0682 3.0682C9.89246 3.24393 9.60754 3.24393 9.4318 3.0682L8 1.6364L8 8.5C8 8.77614 7.77614 9 7.5 9C7.22386 9 7 8.77614 7 8.5L7 1.6364Z"
-											fill="currentColor"
-											fill-rule="evenodd"
-											clip-rule="evenodd"
-										/>
-									</svg>
-								</span>
-							</button>
-						</Drawer.Trigger>
+								<title>svg</title>
+								<path
+									d="M3.5 5.00006C3.22386 5.00006 3 5.22392 3 5.50006L3 11.5001C3 11.7762 3.22386 12.0001 3.5 12.0001L11.5 12.0001C11.7761 12.0001 12 11.7762 12 11.5001L12 5.50006C12 5.22392 11.7761 5.00006 11.5 5.00006L10.25 5.00006C9.97386 5.00006 9.75 4.7762 9.75 4.50006C9.75 4.22392 9.97386 4.00006 10.25 4.00006L11.5 4.00006C12.3284 4.00006 13 4.67163 13 5.50006L13 11.5001C13 12.3285 12.3284 13.0001 11.5 13.0001L3.5 13.0001C2.67157 13.0001 2 12.3285 2 11.5001L2 5.50006C2 4.67163 2.67157 4.00006 3.5 4.00006L4.75 4.00006C5.02614 4.00006 5.25 4.22392 5.25 4.50006C5.25 4.7762 5.02614 5.00006 4.75 5.00006L3.5 5.00006ZM7 1.6364L5.5682 3.0682C5.39246 3.24393 5.10754 3.24393 4.9318 3.0682C4.75607 2.89246 4.75607 2.60754 4.9318 2.4318L7.1818 0.181802C7.26619 0.09741 7.38065 0.049999 7.5 0.049999C7.61935 0.049999 7.73381 0.09741 7.8182 0.181802L10.0682 2.4318C10.2439 2.60754 10.2439 2.89246 10.0682 3.0682C9.89246 3.24393 9.60754 3.24393 9.4318 3.0682L8 1.6364L8 8.5C8 8.77614 7.77614 9 7.5 9C7.22386 9 7 8.77614 7 8.5L7 1.6364Z"
+									fill="currentColor"
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</span>
+					</button>
+					<Drawer.Root
+						open={isOpen}
+						onOpenChange={setIsOpen}
+					>
 						<Drawer.Portal container={container}>
-							<Drawer.Content className="sticky top-0 z-50 h-[140px] w-[95%] cursor-pointer rounded-t-[10px] bg-gray-100 after:h-0 focus:outline-none">
+							<Drawer.Overlay
+								className="fixed inset-0 z-10 bg-black/30"
+								onClick={() => setIsOpen(false)}
+							/>
+							<Drawer.Content className="fixed inset-x-0 bottom-[140px] z-50 h-[140px] max-w-xl cursor-pointer rounded-t-[10px] bg-gray-100 after:h-0 focus:outline-none">
 								{/* <Drawer.Handle className="bg-gray-300" /> */}
-								<Drawer.Close
-									asChild
-									className="fixed inset-x-3 top-2.5 cursor-pointer"
-								>
-									<button type="button">
+								<div className="fixed inset-x-3 top-2.5 cursor-pointer">
+									<button
+										type="button"
+										onClick={() =>
+											setIsOpen(false)
+										}
+									>
 										<svg
 											className=" text-gray-500"
 											width="18"
@@ -136,7 +141,7 @@ const Airbnbsharebookmarks = () => {
 											/>
 										</svg>
 									</button>
-								</Drawer.Close>
+								</div>
 								<div className="relative z-50 flex h-24 w-full items-center justify-center">
 									<AnimatePresence>
 										{images.map((img, idx) => (
@@ -164,7 +169,10 @@ const Airbnbsharebookmarks = () => {
 												fetchPriority="high"
 												alt=""
 												placeholder="blur"
-												src={img}
+												blurDataURL={
+													img.blurDataURL
+												}
+												src={img.src}
 											/>
 										))}
 									</AnimatePresence>
@@ -182,14 +190,15 @@ const Airbnbsharebookmarks = () => {
 											Learn more
 										</span>
 									</p>
-									<Drawer.Close asChild>
-										<button
-											type="button"
-											className="z-50 w-11/12 rounded-lg bg-black px-4 py-2 text-sm text-white hover:bg-black/70"
-										>
-											Got it
-										</button>
-									</Drawer.Close>
+									<button
+										onClick={() =>
+											setIsOpen(false)
+										}
+										type="button"
+										className="z-50 w-11/12 rounded-lg bg-black px-4 py-2 text-sm text-white hover:bg-black/70"
+									>
+										Got it
+									</button>
 								</div>
 							</Drawer.Content>
 							{/* <Drawer.Overlay className="bg-black/50" /> */}
@@ -268,62 +277,61 @@ const Airbnbsharebookmarks = () => {
 						</div>
 					</div>
 				</div>
-				<div className="absolute">
-					<div className="relative mt-24 ml-4 h-96 w-5/6">
-						<Image
-							width={1920} // Original image width
-							height={1080} // Original image height
-							alt=""
-							loading="eager"
-							quality={100}
-							fetchPriority="high"
-							className="rounded-xl object-cover"
-							placeholder="blur"
-							src={Mike}
-						/>
-						<div className="absolute inset-0">
-							<div className="flex items-center gap-24 p-2">
-								<div className="flex items-center justify-center rounded-2xl bg-gray-500 px-3 py-2 text-white text-xs">
-									Superhost
-								</div>
-								<button
-									type="button"
-									className="flex gap-1 rounded-full bg-black px-4 py-3 text-white text-xs"
-								>
-									<span>Map</span>
-									<svg
-										width="15"
-										height="15"
-										viewBox="0 0 15 15"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<title>svg</title>
-										<path
-											d="M9 2H6V13H9V2ZM10 2V13H12.5C12.7761 13 13 12.7761 13 12.5V2.5C13 2.22386 12.7761 2 12.5 2H10ZM2.5 2H5V13H2.5C2.22386 13 2 12.7761 2 12.5V2.5C2 2.22386 2.22386 2 2.5 2ZM2.5 1C1.67157 1 1 1.67157 1 2.5V12.5C1 13.3284 1.67157 14 2.5 14H12.5C13.3284 14 14 13.3284 14 12.5V2.5C14 1.67157 13.3284 1 12.5 1H2.5Z"
-											fill="currentColor"
-											fill-rule="evenodd"
-											clip-rule="evenodd"
-										/>
-									</svg>
-								</button>
+
+				<div className="relative mt-24 ml-4 h-96 w-5/6">
+					<Image
+						width={1920} // Original image width
+						height={1080} // Original image height
+						alt=""
+						loading="eager"
+						quality={100}
+						fetchPriority="high"
+						className="rounded-xl object-cover"
+						placeholder="blur"
+						src={Mike}
+					/>
+					<div className="absolute inset-0">
+						<div className="flex items-center gap-24 p-2">
+							<div className="flex items-center justify-center rounded-2xl bg-gray-500 px-3 py-2 text-white text-xs">
+								Superhost
+							</div>
+							<button
+								type="button"
+								className="flex gap-1 rounded-full bg-black px-4 py-3 text-white text-xs"
+							>
+								<span>Map</span>
 								<svg
-									className=" mr-1 ml-auto"
-									width="25"
-									height="25"
+									width="15"
+									height="15"
 									viewBox="0 0 15 15"
-									fill="white"
+									fill="none"
 									xmlns="http://www.w3.org/2000/svg"
 								>
 									<title>svg</title>
 									<path
-										d="M1.35248 4.90532C1.35248 2.94498 2.936 1.35248 4.89346 1.35248C6.25769 1.35248 6.86058 1.92336 7.50002 2.93545C8.13946 1.92336 8.74235 1.35248 10.1066 1.35248C12.064 1.35248 13.6476 2.94498 13.6476 4.90532C13.6476 6.74041 12.6013 8.50508 11.4008 9.96927C10.2636 11.3562 8.92194 12.5508 8.00601 13.3664C7.94645 13.4194 7.88869 13.4709 7.83291 13.5206C7.64324 13.6899 7.3568 13.6899 7.16713 13.5206C7.11135 13.4709 7.05359 13.4194 6.99403 13.3664C6.0781 12.5508 4.73641 11.3562 3.59926 9.96927C2.39872 8.50508 1.35248 6.74041 1.35248 4.90532Z"
-										fill="red"
+										d="M9 2H6V13H9V2ZM10 2V13H12.5C12.7761 13 13 12.7761 13 12.5V2.5C13 2.22386 12.7761 2 12.5 2H10ZM2.5 2H5V13H2.5C2.22386 13 2 12.7761 2 12.5V2.5C2 2.22386 2.22386 2 2.5 2ZM2.5 1C1.67157 1 1 1.67157 1 2.5V12.5C1 13.3284 1.67157 14 2.5 14H12.5C13.3284 14 14 13.3284 14 12.5V2.5C14 1.67157 13.3284 1 12.5 1H2.5Z"
+										fill="currentColor"
 										fill-rule="evenodd"
 										clip-rule="evenodd"
 									/>
 								</svg>
-							</div>
+							</button>
+							<svg
+								className=" mr-1 ml-auto"
+								width="25"
+								height="25"
+								viewBox="0 0 15 15"
+								fill="white"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<title>svg</title>
+								<path
+									d="M1.35248 4.90532C1.35248 2.94498 2.936 1.35248 4.89346 1.35248C6.25769 1.35248 6.86058 1.92336 7.50002 2.93545C8.13946 1.92336 8.74235 1.35248 10.1066 1.35248C12.064 1.35248 13.6476 2.94498 13.6476 4.90532C13.6476 6.74041 12.6013 8.50508 11.4008 9.96927C10.2636 11.3562 8.92194 12.5508 8.00601 13.3664C7.94645 13.4194 7.88869 13.4709 7.83291 13.5206C7.64324 13.6899 7.3568 13.6899 7.16713 13.5206C7.11135 13.4709 7.05359 13.4194 6.99403 13.3664C6.0781 12.5508 4.73641 11.3562 3.59926 9.96927C2.39872 8.50508 1.35248 6.74041 1.35248 4.90532Z"
+									fill="red"
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+								/>
+							</svg>
 						</div>
 					</div>
 				</div>
