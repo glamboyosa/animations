1a44c704522b1f0edeb253a7cd20dc4516aee2ab
glamboyosa <ogbemudiatimothy@gmail.com>
2025-05-18 16:39:04 +0100

    m

--- src/app/12/page.tsx
+++ src/app/12/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'motion/react';
 import Image from 'next/image';
 import { useEffect, useState, useRef } from 'react';
 
